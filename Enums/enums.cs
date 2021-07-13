802bf6fc9388cda1f29d7f8f80e1cb67ae5d45df
Derrick <derrickwitness@gmail.com>
2021-07-13 17:21:46 +0300

    Added enum for approval status

---
+++ Enums/enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HumanResources.Enums
+{
+    //Approval status should be mapped to what Dynamics Business Central/NAV have for neat code!
+    enum ApprovalStatus
+    {
+        Created = 0,
+        Open = 1,
+        Canceled = 2,
+        Approved = 3,
+        Rejected = 4,
+    }
+}
