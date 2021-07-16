1639fca4def1f5e89e1f0abb3272feb67834771c
Derrick <derrickwitness@gmail.com>
2021-07-16 15:55:50 +0300

    Added Google Calendar API call to get list of Holidays dynamically

---
+++ Controllers/SettingsController.cs
@@ -0,0 +1,47 @@
+﻿using HumanResources.Models;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HumanResources.Controllers
+{
+    public class SettingsController : Controller
+    {
+        // GET: Settings
+        public ActionResult Index()
+        {
+            var client = new RestClient("https://www.googleapis.com/calendar/v3/calendars/en.ke%23holiday%40group.v.calendar.google.com/events?key=AIzaSyBQF1TR_4h4mFj9UZMOl07GRJbH_SiRxxY");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            //Console.WriteLine(response.Content);
+
+            string jsonGoogleCal = response.Content;
+
+            GoogleCalendarAPI jsonGetMemberInfo = JsonConvert.DeserializeObject<GoogleCalendarAPI>(jsonGoogleCal);
+
+            List<PublicHolidays> _PublicHolidays = new List<PublicHolidays>();
+
+            foreach (var holiday in jsonGetMemberInfo.items)
+            {
+                _PublicHolidays.Add(new PublicHolidays { HolidayDate = Convert.ToDateTime(holiday.start.date), Id = holiday.id, Name = holiday.summary });
+                //Console.WriteLine(holiday.id + " - " + holiday.summary +" - "+ holiday.start.date);
+            }
+            // = MembershipNumber;
+
+            List<PublicHolidays> sortedList = _PublicHolidays.Where(f => f.HolidayDate > DateTime.Now).OrderBy(o => o.HolidayDate).ToList();
+            //  Console.WriteLine((int)ElectionStatus.Created);
+
+            foreach (var holday in sortedList)
+            {
+                Console.WriteLine("Holiday Date " + holday.HolidayDate); //insert to Database here
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Models/GoogleCalendarAPI.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HumanResources.Models
+{
+    
+    public class PublicHolidays
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public DateTime HolidayDate { get; set; }
+    }
+
+    public class GoogleCalendarAPI
+    {
+        public string kind { get; set; }
+        public string etag { get; set; }
+        public string summary { get; set; }
+        public DateTime updated { get; set; }
+        public string timeZone { get; set; }
+        public string accessRole { get; set; }
+        public object[] defaultReminders { get; set; }
+        public string nextSyncToken { get; set; }
+        public Item[] items { get; set; }
+    }
+
+    public class Item
+    {
+        public string kind { get; set; }
+        public string etag { get; set; }
+        public string id { get; set; }
+        public string status { get; set; }
+        public string htmlLink { get; set; }
+        public DateTime created { get; set; }
+        public DateTime updated { get; set; }
+        public string summary { get; set; }
+        public Creator creator { get; set; }
+        public Organizer organizer { get; set; }
+        public Start start { get; set; }
+        public End end { get; set; }
+        public string transparency { get; set; }
+        public string visibility { get; set; }
+        public string iCalUID { get; set; }
+        public int sequence { get; set; }
+        public string eventType { get; set; }
+    }
+
+    public class Creator
+    {
+        public string email { get; set; }
+        public string displayName { get; set; }
+        public bool self { get; set; }
+    }
+
+    public class Organizer
+    {
+        public string email { get; set; }
+        public string displayName { get; set; }
+        public bool self { get; set; }
+    }
+
+    public class Start
+    {
+        public string date { get; set; }
+    }
+
+    public class End
+    {
+        public string date { get; set; }
+    }
+
+}
