b77f43ff1ea50dd0f7e1118bfb76a8dec4c6e456
Derrick <derrickwitness@gmail.com>
2021-08-03 11:08:27 +0300

    Added view for edit and view leave type details

--- Controllers/LeaveTypesController.cs
+++ Controllers/LeaveTypesController.cs
@@ -1,4 +1,5 @@
 ﻿using HumanResources.Models;
+using HumanResources.ViewModels;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -72,32 +73,62 @@ namespace HumanResources.Controllers
             _AnnualLeaveDaysType.Add(new AnnualLeaveDaysType { Code = "Working Days", Description = "Working Days" });
             _AnnualLeaveDaysType.Add(new AnnualLeaveDaysType { Code = "Consecutive Days", Description = "Consecutive Days" });
 
-            //
+            
             List<UnitOfMeasure> _UnitOfMeasure = new List<UnitOfMeasure>();
             _UnitOfMeasure.Add(new UnitOfMeasure { Code = "DAY", Description = "Days" });
+
+            var _LeaveTypeViewModel = new LeaveTypeViewModel();
+
+            try
+            {
+                using(var db = new LeaveManagementEntities())
+                {
+                    var leavetype = db.LeaveTypes.Where(x => x.Code == Id).FirstOrDefault();
+                    if(leavetype != null)
+                    {
+                        _LeaveTypeViewModel.OldCode = Id;
+                        _LeaveTypeViewModel.Code = leavetype.Code;
+                        _LeaveTypeViewModel.TotalAbsenceDays = leavetype.TotalAbsence.ToString();
+                        _LeaveTypeViewModel.Description = leavetype.Description;
+                        _LeaveTypeViewModel.UnitofMeasure = leavetype.UnitOfMeasure;
+                        _LeaveTypeViewModel.AnnualLeaveDaysType = leavetype.AnnualLeaveDaysType;
+                    }
+                }
+            }
+            catch(Exception es)
+            {
+                AppFunctions.WriteLog(es.Message);
+            }
 
 
             ViewBag.UnitofMeasure = _UnitOfMeasure;
             ViewBag.AnnualLeaveDaysType = _AnnualLeaveDaysType;
-            return View();
+            return View(_LeaveTypeViewModel);
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult UpdateLeaveType(ViewModels.LeaveTypeViewModel _LeaveTypeViewModel)
+        public ActionResult UpdateLeaveType(ViewModels.LeaveTypeViewModel leavetype)
         {
             string message = null, status = null;
 
             try
-            {
-                var leavetype = new LeaveType { Code = _LeaveTypeViewModel.Code, Description = _LeaveTypeViewModel.Description, TotalAbsence = Convert.ToInt16(_LeaveTypeViewModel.TotalAbsenceDays), AnnualLeaveDaysType = _LeaveTypeViewModel.AnnualLeaveDaysType, UnitOfMeasure = _LeaveTypeViewModel.UnitofMeasure };
-
+            {               
                 using (var dbEntities = new LeaveManagementEntities())
                 {
-                    dbEntities.Configuration.ValidateOnSaveEnabled = false;
-                    dbEntities.LeaveTypes.Add(leavetype);
-                    dbEntities.SaveChanges();
-                    status = "000";
-                    message = "Leave type updated successfully";
+                    var _LeaveType = dbEntities.LeaveTypes.Where(x => x.Code == leavetype.OldCode).FirstOrDefault();
+
+                    if (_LeaveType != null)
+                    {
+                        _LeaveType.Code = leavetype.Code;
+                        _LeaveType.TotalAbsence = Convert.ToInt32(leavetype.TotalAbsenceDays);
+                        _LeaveType.Description = leavetype.Description;
+                        _LeaveType.UnitOfMeasure = leavetype.UnitofMeasure;
+                        _LeaveType.AnnualLeaveDaysType = leavetype.AnnualLeaveDaysType;
+                        dbEntities.SaveChanges();
+
+                        status = "000";
+                        message = "Leave type updated successfully";
+                    }
                 }
             }
             catch (Exception es)
@@ -124,6 +155,29 @@ namespace HumanResources.Controllers
             //
             List<UnitOfMeasure> _UnitOfMeasure = new List<UnitOfMeasure>();
             _UnitOfMeasure.Add(new UnitOfMeasure { Code = "DAY", Description = "Days" });
+
+            var _LeaveTypeViewModel = new LeaveTypeViewModel();
+
+            try
+            {
+                using (var db = new LeaveManagementEntities())
+                {
+                    var leavetype = db.LeaveTypes.Where(x => x.Code == Id).FirstOrDefault();
+                    if (leavetype != null)
+                    {
+                        _LeaveTypeViewModel.OldCode = Id;
+                        _LeaveTypeViewModel.Code = leavetype.Code;
+                        _LeaveTypeViewModel.TotalAbsenceDays = leavetype.TotalAbsence.ToString();
+                        _LeaveTypeViewModel.Description = leavetype.Description;
+                        _LeaveTypeViewModel.UnitofMeasure = leavetype.UnitOfMeasure;
+                        _LeaveTypeViewModel.AnnualLeaveDaysType = leavetype.AnnualLeaveDaysType;
+                    }
+                }
+            }
+            catch (Exception es)
+            {
+                AppFunctions.WriteLog(es.Message);
+            }
 
 
             ViewBag.UnitofMeasure = _UnitOfMeasure;
--- ViewModels/LeaveTypeViewModel.cs
+++ ViewModels/LeaveTypeViewModel.cs
@@ -8,6 +8,7 @@ namespace HumanResources.ViewModels
     public class LeaveTypeViewModel
     {
         public string Code { get; set; }
+        public string OldCode { get; set; }
         public string Description { get; set; }
         public string TotalAbsenceDays { get; set; }
         public string UnitofMeasure { get; set; }
